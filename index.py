48141983d6badaaa03a0040798e7f7a33a62c2bf
tuliocsouzaeng <tulio.goiasa@gmail.com>
2025-12-06 10:41:04 -0300

    Update index.py
    Criação de subdivisões no menu de acordo atualização do Streamlit.

--- index.py
+++ index.py
@@ -1,29 +1,36 @@
 # index.py
 import streamlit as st
 from login import show_login
-
 # Configuração da página
 st.set_page_config(page_title="Goiasa Next", layout="wide")
-
+st.write("Versão do Streamlit:", st.__version__)
 # Verifica se o usuário está autenticado
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
+
+pages = {
+    "Menu":[
+        st.Page("pages/home.py", title="Página Inicial"),
+    ],
+    "Estáticos": [
+        st.Page("pages/tanques.py", title="Tanques"),
+        st.Page("pages/vasos_de_pressao.py", title="Vasos de Pressão"),
+        st.Page("pages/valvulas_de_seguranca.py", title="Válvulas de Segurança"),
+        st.Page("pages/tubulacoes.py", title="Tubulações"),
+        st.Page("pages/torres_de_resfriamento.py", title="Torres de Resfriamento"),
+    ],
+    "Elétricos":[
+        st.Page("pages/motores.py", title="Motores de Baixa Tensão"),
+    ],
+    "Sair":[
+        st.Page("pages/logout.py", title="Sair"),
+    ]
+}
 
 # Se NÃO estiver logado → mostra tela de login
 if not st.session_state["authenticated"]:
     show_login()
 else:
     # Se estiver logado → mostra navegação
-    pg = st.navigation([
-        st.Page("pages/home.py", title="Página Inicial"),
-        st.Page("pages/tanques.py", title="Tanques"),
-        st.Page("pages/vasos_de_pressao.py", title="Vasos de Pressão"),
-        st.Page("pages/valvulas_de_seguranca.py", title="Válvulas de Segurança"),
-        st.Page("pages/tubulacoes.py", title="Tubulações"),
-        st.Page("pages/torres_de_resfriamento.py", title="Torres de Resfriamento"),
-        st.Page("pages/motores.py", title="Motores de Baixa Tensão"),
-        #st.Page("pages/niveis_de_abordagem.py", title="Níveis de Abordagem"),
-        st.Page("pages/logout.py", title="Sair")
-    ])
-
+    pg = st.navigation([pages])
     pg.run()
